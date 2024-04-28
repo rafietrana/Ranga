f9aa03d2d253dea3bba14ea7db54485c7f1bd380
Zioun <jionkhan0@gmail.com>
2024-04-29 04:21:08 +0600

    solve 404 page issue

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Fade } from 'react-awesome-reveal';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <><Fade><div className='flex justify-center items-center h-screen'>
-        <div className='flex flex-col items-center'>
+  return (
+    <>
+    
+      <Fade>
+      <Helmet>
+          <title>Ranga-404</title>
+        </Helmet>
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex flex-col items-center">
             <img src="https://i.ibb.co/KGfkypW/404.gif" alt="gift" />
-            <Link to="/"><button className='btn btn-neutral'>Back to home</button></Link>
+            <Link to="/">
+              <button className="btn btn-neutral">Back to home</button>
+            </Link>
+          </div>
         </div>
-    </div></Fade></>
-    );
+      </Fade>
+    </>
+  );
 };
 
 export default ErrorPage;
